0308959e758e5bd655076ab99b5a34e277a29d45
Donutz <do12nut06@gmail.com>
2022-03-07 00:22:53 +0700

    change to heroku

--- db.js
+++ db.js
@@ -1,18 +1,18 @@
 const Pool = require("pg").Pool;
 
-const pool = new Pool({
-  user: "postgres",
-  password: "admin",
-  database: "bsp",
-  host: "localhost",
-  port: "5432",
-});
-
 // const pool = new Pool({
-//   connectionString: process.env.DATABASE_URL,
-//   ssl: {
-//     rejectUnauthorized: false,
-//   },
+//   user: "postgres",
+//   password: "admin",
+//   database: "bsp",
+//   host: "localhost",
+//   port: "5432",
 // });
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
 
 module.exports = pool;
