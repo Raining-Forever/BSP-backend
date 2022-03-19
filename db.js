12d554992f60556624cb5d0fb2416796adfe45eb
Donutz <do12nut06@gmail.com>
2022-03-19 19:17:29 +0700

    update db

--- db.js
+++ db.js
@@ -10,7 +10,9 @@ const Pool = require("pg").Pool;
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: process.env.DATABASE_URL ? true : false,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 module.exports = pool;
