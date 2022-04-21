f058769575e940c366e1c49274063ae886272fd0
Donutz <do12nut06@gmail.com>
2022-04-21 23:13:34 +0700

    change request method

--- src/appointment/router.js
+++ src/appointment/router.js
@@ -3,7 +3,7 @@ const controller = require("./controller");
 const { Router } = require("express");
 const router = Router();
 
-router.get("/", controller.getAppointments);
+router.put("/", controller.getAppointments);
 router.post("/", controller.addAppointment);
 router.get("/:id", controller.getAppointmentById);
 router.put("/:id", controller.updateAppointment);
